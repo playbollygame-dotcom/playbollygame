033220d0a84f265f59bd4e989dcdb63fa92a8020
ranibharti04 <ranibharti371@gmail.com>
2026-07-11 13:29:32 +0530

    Add games page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -40,12 +40,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
 
-    {
-      url: `${baseUrl}/games`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
+
+     {
+  url: `${baseUrl}/games`,
+  lastModified: new Date(),
+  changeFrequency: "weekly",
+  priority: 0.8,
+},
+  
 
     {
       url: `${baseUrl}/blogs`,
